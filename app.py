e3e6fc8b26a36b3bcf60b0eaf8e08317c8d432c6
Your Name <darii.mi.new@gmail.com>
2024-02-26 09:59:21 +0200

    merge feat-mul

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ def subtract(num1, num2):
     return num1 - num2
 
 
+def multiply(num1, num2):
+    return num1 * num2
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("num1", type=float)
-parser.add_argument("operation", choices=["+", "-"])
+parser.add_argument("operation", choices=["+", "-", "*"])
 parser.add_argument("num2", type=float)
 args = parser.parse_args()
 
@@ -21,3 +25,6 @@ if args.operation == "+":
 elif args.operation == "-":
     result = subtract(args.num1, args.num2)
     print(f"{args.num1} - {args.num2} = {result}")
+elif args.operation == "*":
+    result = multiply(args.num1, args.num2)
+    print(f"{args.num1} * {args.num2} = {result}")
