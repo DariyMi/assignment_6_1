d51f3e4d5418c7900d6530bd54c34b299ea5b25d
Your Name <darii.mi.new@gmail.com>
2024-02-26 09:23:43 +0200

    +divide function

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ def subtract(num1, num2):
     return num1 - num2
 
 
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero"
+    return num1 / num2
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("num1", type=float)
-parser.add_argument("operation", choices=["+", "-"])
+parser.add_argument("operation", choices=["+", "-", "/"])
 parser.add_argument("num2", type=float)
 args = parser.parse_args()
 
@@ -21,3 +27,6 @@ if args.operation == "+":
 elif args.operation == "-":
     result = subtract(args.num1, args.num2)
     print(f"{args.num1} - {args.num2} = {result}")
+elif args.operation == "/":
+    result = divide(args.num1, args.num2)
+    print(f"{args.num1}  {args.num2} = {result}")
